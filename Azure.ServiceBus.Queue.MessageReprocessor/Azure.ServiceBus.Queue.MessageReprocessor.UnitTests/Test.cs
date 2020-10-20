c6563535a5addf1bd04556bbe5755868a28cc299
Pavel <smirnov_pasha@mail.ru>
2020-10-21 01:04:23 +0300

    Initial commit

---
+++ Azure.ServiceBus.Queue.MessageReprocessor/Azure.ServiceBus.Queue.MessageReprocessor.UnitTests/Test.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Azure.ServiceBus.Queue.MessageReprocessor.UnitTests
+{
+    public class Test
+    {
+        [Fact]
+        public void Fact()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Azure.ServiceBus.Queue.MessageReprocessor/Azure.ServiceBus.Queue.MessageReprocessor/IRetryDelayStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Azure.ServiceBus.Queue.MessageReprocessor
+{
+    public interface IRetryDelayStrategy
+    {
+        bool CanDelay(int attempt);
+        TimeSpan GetDelay(int attempt);
+    }
+}
